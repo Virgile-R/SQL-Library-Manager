2d46374f85f944def01a2a2620ba20f75a351cac
Virgile Reiter <virgile.reiter@gmail.com>
2021-10-12 16:14:20 +0200

    first version with CRUD functions

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const sequelize = require('./models').sequelize
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,22 +20,36 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404, 'Page not found'));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
+  res.locals.message = err.message || "Something went wrong...";
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  if (err.status === 404){
+    res.render("pagenotfound", {err})
+  } else {
+    console.error(err.status + ': ' + err.message)
+    res.render('error', {err});
+}
 });
-
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Lessa go');
+    await sequelize.sync()
+    console.log('database synced')
+  } catch (error){  
+    console.error('RIP' + error)
+  }
+})();
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,113 @@
 var express = require('express');
 var router = express.Router();
+const Book = require("../models").Book
 
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      // Forward error to the global error handler
+      next(error);
+    }
+  }
+}
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', asyncHandler( async (req, res, next) => {
+  res.redirect("/books")
+}));
+/**
+ * Get book list
+ */
+router.get('/books', asyncHandler( async (req, res, next) => {
+  const books = await Book.findAll()
+  res.render('index', {books, title: "Book List"})
+}))
+/**
+ * Form for creating new books
+ */
+router.get('/books/new', asyncHandler(async (req, res, next) => {
+  res.render('newbook', {article: {}, title: "Enter new book"})
+}))
 
+/**
+ * Post method for creating books
+ */
+router.post('/books/new', asyncHandler(async (req, res) => {
+  let book
+  try {
+    const book = await Book.create(req.body)
+    res.redirect('/books/' + book.id)
+  } catch (error) {
+    if(error.name === "SequelizeValidationError"){
+      book = await Book.build(req.body)
+      res.render('newbook', {book, errors: error.errors, title: 'Enter new book'})
+    }
+  }
+}))
+/**
+ * Route for individual book
+ */
+router.get('/books/:id', asyncHandler(async (req, res) => {
+  const book = await Book.findByPk(req.params.id)
+  if (book){
+    res.render("book", {book, title: book.title})
+  } else {
+    res.sendStatus(404)
+  }
+}))
+/**
+ * Form for editing books
+ */
+router.get('/books/:id/edit', asyncHandler(async (req, res) => {
+  const book = await Book.findByPk(req.params.id)
+  if (book){
+    res.render('bookEdit', {book, title: "Edit book"})
+  } else {
+    res.sendStatus(404)
+  }
+}))
+/**
+ * Post method for editing books
+ */
+router.post('/books/:id', asyncHandler(async (req, res) => {
+  let book
+  try {
+    book = await Book.findByPk(req.params.id);
+    if (book){
+      await book.update(req.body)
+      res.redirect('/books/' + book.id)
+    }else {
+      res.sendStatus(404)
+    }
+    
+  } catch (error) {
+    
+    if(error.name === "SequelizeValidationError"){
+      
+      book = await Book.build(req.body)
+      res.render('bookEdit', {book, errors: error.errors, title: 'Edit book'})
+    } else {
+      throw error
+    }
+  }
+}))
+
+
+/**
+ * Post method for deleting books
+ */
+router.post('/books/:id/delete', asyncHandler(async (req, res) => {
+  const book = await Book.findByPk(req.params.id)
+  if (book){
+    
+    await book.destroy()
+    res.redirect("/")
+  } else {
+    
+    res.sendStatus(404)
+  }
+  
+}))
 module.exports = router;
